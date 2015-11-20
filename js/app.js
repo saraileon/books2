2163b18160e189fde2e0e4546a52370a0a15c544
Sarai Leon <sleon90@gmail.com>
2015-11-20 14:24:49 -0600

    Slider

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-/*--global alert:true*/
+/*--global console:true*/
 
 var loader = function(){
  var h = $(window).height();
@@ -10,12 +10,22 @@ var alignBooks = function () {
 	$('.ebook-list > .col-sm-2 > a > img').each( function () {
 		var p =		$(this).parents('.ebook-list');
 		var rh =	$(p).height();
+		// $(this).siblings('.cover-overlay').css({'bottom' : '-'+rh+'px'});
 		var h = 	( rh - $(this).height() ) ;
 
 		$(this).css({'margin-top' : h+'px'});
 	});
 };
 
+var getTitles = function () {
+	$('.ebook-list > .col-sm-2 > a > img').each( function () {
+		var t = $(this).attr('data-title');
+		var s = $(this).siblings('.cover-overlay');
+		var h = $(this).attr('src');
+		$(this).parent('a').attr({'title':t, 'href':h});
+		$(s).children('p').html(t);
+	});
+};
 // loader();
 
 $(document).ready(function() {
@@ -26,6 +36,7 @@ $(window).load( function () {
 	$('#loader').fadeOut(2800);
 
 	alignBooks();
+	getTitles();
 	$(window).resize(alignBooks);
 	
 });
